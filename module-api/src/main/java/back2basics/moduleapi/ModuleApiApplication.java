c08a168535a5380e61778a5f2d6b8b20bbc03bad
ooyniz <ooyniz@gmail.com>
2025-05-20 13:49:48 +0900

    chore: 불필요한 모듈 의존성 제거, scanBasePackages 범위 수정

--- module-api/src/main/java/back2basics/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/back2basics/moduleapi/ModuleApiApplication.java
@@ -3,13 +3,15 @@ package back2basics.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (
-        scanBasePackages = {"back2basics.moduleapi", "back2basics.modulecommon"}
-)
+@SpringBootApplication(scanBasePackages = {
+    "back2basics.moduleapi",
+    "back2basics.moduleservice",
+    "back2basics.modulecommon"
+})
 public class ModuleApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ModuleApiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ModuleApiApplication.class, args);
+  }
 
 }
