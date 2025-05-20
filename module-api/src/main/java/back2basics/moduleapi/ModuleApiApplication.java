a948850dbd5e8b45eb7db816ab24364221fa6d40
ooyniz <ooyniz@gmail.com>
2025-05-20 14:56:51 +0900

    fix: 스캔 범위 수정

--- module-api/src/main/java/back2basics/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/back2basics/moduleapi/ModuleApiApplication.java
@@ -2,12 +2,17 @@ package back2basics.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
     "back2basics.moduleapi",
+    "back2basics.moduleinfra",
     "back2basics.moduleservice",
     "back2basics.modulecommon"
 })
+@EntityScan("back2basics.moduledomain")
+@EnableJpaRepositories("back2basics.moduledomain")
 public class ModuleApiApplication {
 
   public static void main(String[] args) {
