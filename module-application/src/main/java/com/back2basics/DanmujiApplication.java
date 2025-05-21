5a48f9e5794db26bd076fe31171e3cbf07ddb0c3
ooyniz <ooyniz@gmail.com>
2025-05-21 15:49:17 +0900

    init: Initial Commit

---
+++ module-application/src/main/java/com/back2basics/DanmujiApplication.java
@@ -0,0 +1,12 @@
+package com.back2basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanmujiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DanmujiApplication.class, args);
+  }
+}
