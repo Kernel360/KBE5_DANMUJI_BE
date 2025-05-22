58fdf5b3497e71ce5f6c78c1b417d38fee7b7eb2
swlee <swo98@naver.com>
2025-05-22 14:47:49 +0900

    feat: Post 도메인 객체 생성

---
+++ module-core/src/main/java/com/back2basics/model/post/Post.java
@@ -0,0 +1,33 @@
+package com.back2basics.model.post;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Post {
+
+    private Long id;
+    private String authorName;
+    private String title;
+    private String content;
+
+    @Builder
+    public Post(Long id, String authorName, String title, String content) {
+        this.id = id;
+        this.authorName = authorName;
+        this.title = title;
+        this.content = content;
+    }
+
+    // todo : Post 관련 CustomException 추가 필요
+    public void isAuthor(String requesterName) {
+        if (!this.authorName.equals(requesterName)) {
+            throw new IllegalStateException("작성자만 수정 또는 삭제할 수 있습니다.");
+        }
+    }
+
+    public void updateContent(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
