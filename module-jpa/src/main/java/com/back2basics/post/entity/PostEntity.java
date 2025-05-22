9fd16823dffb26236b82a533f3b03d42fda96b68
swlee <swo98@naver.com>
2025-05-22 14:42:54 +0900

    feat: PostEntity, Mapper 추가

---
+++ module-jpa/src/main/java/com/back2basics/post/entity/PostEntity.java
@@ -0,0 +1,42 @@
+package com.back2basics.post.entity;
+
+import com.back2basics.common.entity.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "post")
+// todo : ERD에 기반한 따른 필드 값 추가
+public class PostEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author_name", nullable = false)
+    private String authorName;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public PostEntity(Long id, String authorName, String title, String content) {
+        this.id = id;
+        this.authorName = authorName;
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ module-jpa/src/main/java/com/back2basics/post/mapper/PostMapper.java
@@ -0,0 +1,25 @@
+package com.back2basics.post.mapper;
+
+import com.back2basics.post.entity.PostEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+
+    public Post toDomain(PostEntity entity) {
+        return Post.builder()
+            .id(entity.getId())
+            .authorName(entity.getAuthorName())
+            .title(entity.getTitle())
+            .content(entity.getContent())
+            .build();
+    }
+
+    public PostEntity fromDomain(Post domain) {
+        return PostEntity.builder()
+            .authorName(domain.getAuthorName())
+            .title(domain.getTitle())
+            .content(domain.getContent())
+            .build();
+    }
+}
