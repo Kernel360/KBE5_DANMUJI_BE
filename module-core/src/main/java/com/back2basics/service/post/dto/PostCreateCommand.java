792f85ce8609b24a0eca1dae20381cefcf4f75f0
swlee <swo98@ajou.ac.kr>
2025-05-24 18:39:23 +0900

    refactor: 어노테이션 오타 수정 (#27)

--- module-core/src/main/java/com/back2basics/service/post/dto/PostCreateCommand.java
+++ module-core/src/main/java/com/back2basics/service/post/dto/PostCreateCommand.java
@@ -1,5 +1,6 @@
 package com.back2basics.service.post.dto;
 
+import com.back2basics.infra.post.custom.CustomEnumCheck;
 import com.back2basics.model.post.PostType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,7 +22,7 @@ public class PostCreateCommand {
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
 
-    @CustomEnumValidator(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
+    @CustomEnumCheck(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
     private PostType type;
 
     @NotNull(message = "우선순위가 입력되지 않았습니다.")
--- module-core/src/main/java/com/back2basics/service/post/dto/PostUpdateCommand.java
+++ module-core/src/main/java/com/back2basics/service/post/dto/PostUpdateCommand.java
@@ -1,5 +1,6 @@
 package com.back2basics.service.post.dto;
 
+import com.back2basics.infra.post.custom.CustomEnumCheck;
 import com.back2basics.infra.post.custom.CustomTitleNotBlank;
 import com.back2basics.model.post.PostStatus;
 import com.back2basics.model.post.PostType;
@@ -21,10 +22,10 @@ public class PostUpdateCommand {
     @Size(min = 1, message = "내용은 공백일 수 없습니다.")
     private String content;
 
-    @CustomEnumValidator(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
+    @CustomEnumCheck(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
     private PostType type;
 
-    @CustomEnumValidator(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
+    @CustomEnumCheck(enumClass = PostType.class, message = "올바른 enum type이 아닙니다")
     private PostStatus status;
 
     private Integer priority;
