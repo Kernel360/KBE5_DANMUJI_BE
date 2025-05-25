593de056b3fb2f276c945d30f29ad334a8a4e4d9
swlee <swo98@ajou.ac.kr>
2025-05-25 17:04:07 +0900

    feat: 컨트롤러, 응답코드 구현 (#38)

---
+++ module-web/src/main/java/com/back2basics/comment/controller/CommentResponseCode.java
@@ -0,0 +1,18 @@
+package com.back2basics.comment.controller;
+
+import com.back2basics.response.global.code.ResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommentResponseCode implements ResponseCode {
+
+    COMMENT_CREATE_SUCCESS(HttpStatus.CREATED, "C201", "댓글 생성 완료"),
+    COMMENT_DELETE_SUCCESS(HttpStatus.OK, "C202", "댓글 삭제 완료");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
