5e82621207ad706ccf2a0c13d6c4cef96c91bbe9
swlee <swo98@naver.com>
2025-05-26 12:31:12 +0900

    refactor: jpaAdapter에서 연관관계 설정 로직 분리 (#38) (#39)

--- module-jpa/src/main/java/com/back2basics/comment/adapter/out/CommentJpaAdapter.java
+++ module-jpa/src/main/java/com/back2basics/comment/adapter/out/CommentJpaAdapter.java
@@ -5,12 +5,6 @@ import com.back2basics.comment.mapper.CommentMapper;
 import com.back2basics.comment.repository.CommentEntityRepository;
 import com.back2basics.model.comment.Comment;
 import com.back2basics.port.out.comment.CommentRepositoryPort;
-import com.back2basics.post.entity.PostEntity;
-import com.back2basics.post.repository.PostEntityRepository;
-import com.back2basics.service.comment.exception.CommentErrorCode;
-import com.back2basics.service.comment.exception.CommentException;
-import com.back2basics.service.post.exception.PostErrorCode;
-import com.back2basics.service.post.exception.PostException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -20,21 +14,14 @@ import org.springframework.stereotype.Component;
 public class CommentJpaAdapter implements CommentRepositoryPort {
 
     private final CommentEntityRepository commentRepository;
-    private final PostEntityRepository postEntityRepository;
+    private final CommentRelationAdapter commentRelationAdapter;
     private final CommentMapper mapper;
 
     @Override
     public Long save(Comment comment) {
         CommentEntity entity = mapper.fromDomain(comment);
-
-        PostEntity postEntity = findPostEntity(comment.getPostId());
-        entity.assignPost(postEntity);
-
-        if (comment.getParentCommentId() != null) {
-            CommentEntity parentEntity = findParentEntity(comment.getParentCommentId());
-            entity.assignParentComment(parentEntity);
-            parentEntity.addChildComment(entity);
-        }
+        commentRelationAdapter.assignRelations(entity, comment.getPostId(),
+            comment.getParentCommentId());
 
         return commentRepository.save(entity).getId();
     }
@@ -47,14 +34,8 @@ public class CommentJpaAdapter implements CommentRepositoryPort {
     @Override
     public void update(Comment comment) {
         CommentEntity entity = mapper.fromDomain(comment);
-
-        PostEntity postEntity = findPostEntity(comment.getPostId());
-        entity.assignPost(postEntity);
-
-        if (comment.getParentCommentId() != null) {
-            CommentEntity parentEntity = findParentEntity(comment.getParentCommentId());
-            entity.assignParentComment(parentEntity);
-        }
+        commentRelationAdapter.assignRelations(entity, comment.getPostId(),
+            comment.getParentCommentId());
 
         commentRepository.save(mapper.fromDomain(comment));
     }
@@ -64,14 +45,4 @@ public class CommentJpaAdapter implements CommentRepositoryPort {
         commentRepository.delete(mapper.fromDomain(comment));
     }
 
-    public PostEntity findPostEntity(Long postId) {
-        return postEntityRepository.findById(postId)
-            .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
-    }
-
-    public CommentEntity findParentEntity(Long parentId) {
-        return commentRepository.findById(parentId)
-            .orElseThrow(
-                () -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
-    }
 }
