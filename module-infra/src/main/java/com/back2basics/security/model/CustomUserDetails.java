af599582e5a76ee7805c0c011a493e58b241b99e
ooyniz <ooyniz@gmail.com>
2025-05-29 15:51:21 +0900

    refactor: CustomUserDetails에서 User 객체 보유 -> userId, username, password만 포함하도록 수정 (#74)

--- module-infra/src/main/java/com/back2basics/security/model/CustomUserDetails.java
+++ module-infra/src/main/java/com/back2basics/security/model/CustomUserDetails.java
@@ -4,32 +4,39 @@ package com.back2basics.security.model;
 import com.back2basics.user.model.User;
 import java.util.Collection;
 import java.util.List;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
 
-    private final User user;
+    @Getter
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final String role;
 
     public CustomUserDetails(User user) {
-        this.user = user;
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.role = user.getRole().getKey();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        String roleKey = user.getRole() != null ? user.getRole().getKey() : "ROLE_USER";
-        return List.of(new SimpleGrantedAuthority(roleKey));
+        return List.of(new SimpleGrantedAuthority(role));
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return username;
     }
 
     @Override
