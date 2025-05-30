a40d146389d0aff6fdadeaeb36c9c1aa6bdb0b5f
Jinhy00 <pjhark0@gmail.com>
2025-05-30 17:39:32 +0900

    refactor: 세분화된 port로 변경(#88)

--- module-core/src/main/java/com/back2basics/project/port/out/ProjectRepositoryPort.java
+++ module-core/src/main/java/com/back2basics/project/port/out/ProjectRepositoryPort.java
@@ -1,19 +1,19 @@
-package com.back2basics.project.port.out;
-
-import com.back2basics.project.model.Project;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-@Component
-public interface ProjectRepositoryPort {
-
-    void save(Project project);
-
-    Optional<Project> findById(Long id);
-
-    List<Project> findAll();
-
-    void update(Project project);
-}
+//package com.back2basics.project.port.out;
+//
+//import com.back2basics.project.model.Project;
+//import java.util.List;
+//import java.util.Optional;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Repository;
+//
+//@Component
+//public interface ProjectRepositoryPort {
+//
+//    void save(Project project);
+//
+//    Optional<Project> findById(Long id);
+//
+//    List<Project> findAll();
+//
+//    void update(Project project);
+//}
