cd8e0d72705c9f041771132452352376d74aacf4
Jinhy00 <pjhark0@gmail.com>
2025-05-31 00:36:23 +0900

    refactor: component 어노테이션 수정(#88)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/project/adapter/ReadProjectAdapter.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/project/adapter/ReadProjectAdapter.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class ReadProjectAdapter implements ReadProjectPort {
 
