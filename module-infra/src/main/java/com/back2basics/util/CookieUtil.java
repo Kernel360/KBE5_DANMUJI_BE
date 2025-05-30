a2dd6c2297acc9a6429e7efac5861765476ffe22
ooyniz <ooyniz@gmail.com>
2025-05-30 11:49:33 +0900

    feat: Cookie 사용을 위한 CookieUtil 생성 (#74)

---
+++ module-infra/src/main/java/com/back2basics/util/CookieUtil.java
@@ -0,0 +1,30 @@
+package com.back2basics.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    // 쿠키
+    public Cookie createCookie(String value) {
+        Cookie cookie = new Cookie("refreshToken", value);
+        cookie.setMaxAge(24 * 60 * 60);
+        // https 설정 시
+//        cookie.setSecure(true);
+//        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+
+    public void deleteRefreshTokenCookie(HttpServletResponse response) {
+        Cookie deleteCookie = new Cookie("refreshToken", null);
+        deleteCookie.setMaxAge(0);
+        deleteCookie.setPath("/");
+        deleteCookie.setHttpOnly(true);
+        response.addCookie(deleteCookie);
+    }
+}
+
