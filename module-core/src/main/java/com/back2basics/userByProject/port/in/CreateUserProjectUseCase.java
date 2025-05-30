009542557666076405ebb388ca544deaacb49da0
Jinhy00 <pjhark0@gmail.com>
2025-05-30 17:20:10 +0900

    feat: usecase 및 service 생성(#115)

---
+++ module-core/src/main/java/com/back2basics/userByProject/port/in/CreateUserProjectUseCase.java
@@ -0,0 +1,6 @@
+package com.back2basics.userByProject.port.in;
+
+public interface CreateUserProjectUseCase {
+
+    void createUserByProject(Long projectId);
+}
---
+++ module-core/src/main/java/com/back2basics/userByProject/service/CreateUserProjectService.java
@@ -0,0 +1,5 @@
+package com.back2basics.userByProject.service;
+
+public class CreateUserProjectService {
+
+}
