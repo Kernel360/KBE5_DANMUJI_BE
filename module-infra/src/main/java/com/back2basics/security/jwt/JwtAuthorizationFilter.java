5d5485b0002d58169efcf8cefeee6aaee16489fa
ooyniz <ooyniz@gmail.com>
2025-05-30 21:24:56 +0900

    feat: 액세스 토큰 만료 시 slientReissueAccessToken 추가 (#74)

--- module-infra/src/main/java/com/back2basics/security/jwt/JwtAuthorizationFilter.java
+++ module-infra/src/main/java/com/back2basics/security/jwt/JwtAuthorizationFilter.java
@@ -1,7 +1,9 @@
 package com.back2basics.security.jwt;
 
 import static com.back2basics.global.response.code.AuthErrorCode.TOKEN_BLACKLISTED;
+import static com.back2basics.global.response.code.AuthErrorCode.TOKEN_EXPIRED;
 import static com.back2basics.global.response.code.AuthErrorCode.TOKEN_INVALID;
+import static com.back2basics.global.response.code.AuthErrorCode.TOKEN_NOT_FOUND;
 
 import com.back2basics.global.response.error.ErrorResponse;
 import com.back2basics.global.response.result.ApiResponse;
@@ -10,6 +12,7 @@ import com.back2basics.security.exception.InvalidTokenException;
 import com.back2basics.security.model.CustomUserDetails;
 import com.back2basics.user.model.User;
 import com.back2basics.user.port.in.UserQueryUseCase;
+import com.back2basics.util.CookieUtil;
 import com.back2basics.util.RedisUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -31,12 +34,14 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserQueryUseCase userQueryUseCase;
     private final RedisUtil redisUtil;
+    private final CookieUtil cookieUtil;
 
     public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider,
-        UserQueryUseCase userQueryUseCase, RedisUtil redisUtil) {
+        UserQueryUseCase userQueryUseCase, RedisUtil redisUtil, CookieUtil cookieUtil) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.userQueryUseCase = userQueryUseCase;
         this.redisUtil = redisUtil;
+        this.cookieUtil = cookieUtil;
     }
 
     @Override
@@ -50,18 +55,23 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             return;
         }
 
-        try {
-            jwtTokenProvider.validateAccessToken(accessToken);
-        } catch (InvalidTokenException e) {
-            log.error("InvalidTokenException: {}", e.getMessage());
-            response.sendError(TOKEN_INVALID.getStatus().value(), TOKEN_INVALID.getMessage());
-            return;
-        }
-
         if (redisUtil.hasKey("BL:" + accessToken)) {
             ResponseEntity<ApiResponse<ErrorResponse>> apiResponse = ApiResponse.error(
                 TOKEN_BLACKLISTED);
             ResponseUtil.writeJson(response, apiResponse);
+            return;
+        }
+
+        try {
+            jwtTokenProvider.validateAccessToken(accessToken);
+        } catch (InvalidTokenException e) {
+            if (e.getErrorCode().equals(TOKEN_EXPIRED)) {
+                silentReissueAccessToken(accessToken, request, response, filterChain);
+                return;
+            }
+
+            log.error("InvalidTokenException: {}", e.getMessage());
+            response.sendError(TOKEN_INVALID.getStatus().value(), TOKEN_INVALID.getMessage());
             return;
         }
 
@@ -80,4 +90,30 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         return new UsernamePasswordAuthenticationToken(userDetails, null,
             userDetails.getAuthorities());
     }
+
+    private void silentReissueAccessToken(String accessToken, HttpServletRequest request,
+        HttpServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
+        String username = jwtTokenProvider.getSubject(accessToken);
+
+        if (redisUtil.hasKey("RT:" + username)) {
+            User user = userQueryUseCase.findByUsername(username);
+            CustomUserDetails userDetails = new CustomUserDetails(user);
+
+            String newAccessToken = jwtTokenProvider.createAccessToken(userDetails);
+            String newRefreshToken = jwtTokenProvider.createRefreshToken(userDetails);
+
+            response.setHeader("Authorization", "Bearer " + newAccessToken);
+            response.addCookie(cookieUtil.createCookie(newRefreshToken));
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendError(TOKEN_NOT_FOUND.getStatus().value(), TOKEN_NOT_FOUND.getMessage());
+        }
+    }
+
 }
