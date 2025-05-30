2119e6661ffa5d70b85fc1525163173eb43a1bd6
ooyniz <ooyniz@gmail.com>
2025-05-31 02:11:16 +0900

    feat: Redis 연결 실패 시 커스텀 에러 처리 (#132)

--- module-infra/src/main/java/com/back2basics/util/RedisUtil.java
+++ module-infra/src/main/java/com/back2basics/util/RedisUtil.java
@@ -1,9 +1,16 @@
 package com.back2basics.util;
 
+import static com.back2basics.global.response.code.CommonErrorCode.REDIS_CONNECTION_ERROR;
+
+import com.back2basics.security.exception.CustomRedisConnectionException;
+import io.lettuce.core.RedisConnectionException;
 import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class RedisUtil {
 
@@ -14,11 +21,17 @@ public class RedisUtil {
     }
 
     public void save(String key, Object val, Long time, TimeUnit timeUnit) {
-        redisTemplate.opsForValue().set(key, val, time, timeUnit);
+        try {
+            redisTemplate.opsForValue().set(key, val, time, timeUnit);
+        } catch (RedisConnectionFailureException | RedisConnectionException e) {
+            throw new CustomRedisConnectionException(REDIS_CONNECTION_ERROR);
+        } catch (Exception e) {
+            log.error("Failed to save key {} to Redis", key, e);
+        }
     }
 
     public void setDataExpire(String key, String value, long durationMillis) {
-        redisTemplate.opsForValue().set(key, value, durationMillis);
+        redisTemplate.opsForValue().set(key, value, durationMillis, TimeUnit.MILLISECONDS);
     }
 
     public String getData(String key) {
@@ -36,6 +49,14 @@ public class RedisUtil {
 
     public boolean delete(String key) {
         return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+    public String buildRefreshTokenKey(String username) {
+        return "RT:" + username;
+    }
+
+    public String buildBlacklistTokenKey(String accessToken) {
+        return "BL:" + accessToken;
     }
 
 }
