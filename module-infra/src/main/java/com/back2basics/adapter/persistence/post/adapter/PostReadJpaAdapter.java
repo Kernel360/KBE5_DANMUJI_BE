c5f34c39c6964749a8c3523c0ed64eaf249d62a6
swlee <swo98@naver.com>
2025-06-01 21:31:15 +0900

    Revert "[Fix] 대댓글이 중복으로 포함되는 현상 해결"

--- module-infra/src/main/java/com/back2basics/adapter/persistence/post/adapter/PostReadJpaAdapter.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/post/adapter/PostReadJpaAdapter.java
@@ -4,11 +4,9 @@ import static com.back2basics.adapter.persistence.post.QPostEntity.postEntity;
 
 import com.back2basics.adapter.persistence.comment.CommentEntity;
 import com.back2basics.adapter.persistence.comment.CommentEntityRepository;
-import com.back2basics.adapter.persistence.comment.CommentMapper;
 import com.back2basics.adapter.persistence.post.PostEntity;
 import com.back2basics.adapter.persistence.post.PostEntityRepository;
 import com.back2basics.adapter.persistence.post.PostMapper;
-import com.back2basics.comment.model.Comment;
 import com.back2basics.infra.exception.post.PostErrorCode;
 import com.back2basics.infra.exception.post.PostException;
 import com.back2basics.post.model.Post;
@@ -31,7 +29,6 @@ public class PostReadJpaAdapter implements PostReadPort {
     private final CommentEntityRepository commentRepository;
     private final JPAQueryFactory queryFactory;
     private final PostMapper mapper;
-    private final CommentMapper commentMapper;
 
     @Override
     public Optional<Post> findById(Long id) {
@@ -39,8 +36,7 @@ public class PostReadJpaAdapter implements PostReadPort {
             .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
 
         List<CommentEntity> commentEntities = commentRepository.findByPostId(entity.getId());
-        List<Comment> commentTree = commentMapper.toDomainHierarchy(commentEntities);
-        return Optional.of(mapper.toDomain(entity, commentTree));
+        return Optional.of(mapper.toDomain(entity, commentEntities));
     }
 
     @Override
