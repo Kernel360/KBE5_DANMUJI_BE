11173e69b03ccad299ca6328df141fa69cd5008c
swlee <swo98@ajou.ac.kr>
2025-06-01 15:25:21 +0900

    refactor: 댓글 jpa apdater 로직 수정 (#134)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/comment/adapter/CommentDeleteJpaAdapter.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/comment/adapter/CommentDeleteJpaAdapter.java
@@ -5,6 +5,8 @@ import com.back2basics.adapter.persistence.comment.CommentEntityRepository;
 import com.back2basics.adapter.persistence.comment.CommentMapper;
 import com.back2basics.comment.model.Comment;
 import com.back2basics.comment.port.out.CommentDeletePort;
+import com.back2basics.service.comment.exception.CommentErrorCode;
+import com.back2basics.service.comment.exception.CommentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,17 @@ import org.springframework.stereotype.Component;
 public class CommentDeleteJpaAdapter implements CommentDeletePort {
 
     private final CommentEntityRepository commentRepository;
-    private final CommentDeleteHelper commentDeleteHelper;
     private final CommentMapper mapper;
 
     @Override
     public void delete(Comment comment) {
-        CommentEntity entity = mapper.fromDomain(comment);
-        commentDeleteHelper.deleteCommentWithOrphanKeep(entity.getId());
+        CommentEntity entity = commentRepository.findById(comment.getId())
+            .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
+
+        // 자식 댓글들의 부모 참조 제거
+        entity.getChildrenComments().forEach(child ->
+            child.assignParentComment(null));
+        entity.getChildrenComments().clear();
 
         commentRepository.delete(entity);
     }
