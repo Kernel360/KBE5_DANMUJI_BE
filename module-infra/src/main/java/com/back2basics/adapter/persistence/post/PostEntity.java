451dbe7587a7b109375b7b671ead82c548b8a819
swlee <swo98@ajou.ac.kr>
2025-06-01 14:54:45 +0900

    refactor: PostEntity에서 Commentts 필드 제거 (#134)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/post/PostEntity.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/post/PostEntity.java
@@ -1,23 +1,17 @@
 package com.back2basics.adapter.persistence.post;
 
-import com.back2basics.adapter.persistence.comment.CommentEntity;
 import com.back2basics.adapter.persistence.common.entity.BaseTimeEntity;
 import com.back2basics.post.model.PostStatus;
 import com.back2basics.post.model.PostType;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -44,9 +38,6 @@ public class PostEntity extends BaseTimeEntity {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<CommentEntity> comments = new ArrayList<>();
-
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
     private PostType type;
@@ -63,8 +54,7 @@ public class PostEntity extends BaseTimeEntity {
 
     @Builder
     public PostEntity(Long id, Long authorId, String title, String content, PostType type,
-        Integer priority, PostStatus status, LocalDateTime completedAt,
-        List<CommentEntity> comments) {
+        Integer priority, PostStatus status, LocalDateTime completedAt) {
         this.id = id;
         this.authorId = authorId;
         this.title = title;
@@ -73,17 +63,6 @@ public class PostEntity extends BaseTimeEntity {
         this.priority = priority;
         this.status = status;
         this.completedAt = completedAt;
-        this.comments = comments;
-    }
-
-    public void addComment(CommentEntity comment) {
-        comments.add(comment);
-        comment.assignPost(this);
-    }
-
-    public void removeComment(CommentEntity comment) {
-        comments.remove(comment);
-        comment.assignPost(null);
     }
 
     public void update(String title, String content, PostType type, PostStatus status,
