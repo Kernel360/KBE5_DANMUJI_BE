c7e4be1c35a2d2e05da1f74a5d73b6ca2158b2bd
swlee <swo98@naver.com>
2025-06-02 19:44:21 +0900

    docs: 응답값 포함된 상수클래스 생성 (#146)

---
+++ module-api/src/main/java/com/back2basics/domain/question/swagger/QuestionDocsResult.java
@@ -0,0 +1,109 @@
+package com.back2basics.domain.question.swagger;
+
+public class QuestionDocsResult {
+
+    public static final String QUESTION_CREATE_REQUEST = """
+        {
+          "postId": 1,
+          "authorId": 2,
+          "content": "이 게시글에 대해 질문이 있습니다."
+        }
+        """;
+
+    public static final String QUESTION_CREATE_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q001",
+          "message": "질문 생성 성공",
+          "data": 1
+        }
+        """;
+
+    public static final String QUESTION_READ_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q002",
+          "message": "질문 조회 성공",
+          "data": [
+            {
+              "id": 1,
+              "postId": 1,
+              "authorId": 2,
+              "content": "질문 내용입니다.",
+              "status": "WAITING",
+              "createdAt": "2024-01-01T10:00:00",
+              "updatedAt": null,
+              "deletedAt": null,
+              "isDeleted": false
+            }
+          ]
+        }
+        """;
+
+    public static final String QUESTION_UPDATE_REQUEST = """
+        {
+          "requesterId": 2,
+          "content": "수정된 질문 내용입니다."
+        }
+        """;
+
+    public static final String QUESTION_UPDATE_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q003",
+          "message": "질문 수정 성공",
+          "data": null
+        }
+        """;
+
+    public static final String QUESTION_DELETE_REQUEST = """
+        {
+          "requesterId": 2
+        }
+        """;
+
+    public static final String QUESTION_DELETE_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q004",
+          "message": "질문 삭제 성공",
+          "data": null
+        }
+        """;
+
+    public static final String QUESTION_MARK_ANSWERED_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q005",
+          "message": "질문 상태 변경 성공 - 답변완료",
+          "data": null
+        }
+        """;
+
+    public static final String QUESTION_MARK_RESOLVED_SUCCESS = """
+        {
+          "success": true,
+          "code": "Q006",
+          "message": "질문 상태 변경 성공 - 해결됨",
+          "data": null
+        }
+        """;
+
+    public static final String INVALID_QUESTION_AUTHOR = """
+        {
+          "success": false,
+          "code": "Q002",
+          "message": "질문 작성자가 아닙니다",
+          "data": null
+        }
+        """;
+
+    public static final String QUESTION_NOT_FOUND = """
+        {
+          "success": false,
+          "code": "Q001",
+          "message": "질문을 찾을 수 없습니다",
+          "data": null
+        }
+        """;
+}
