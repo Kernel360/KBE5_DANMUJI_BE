419caf3177cd4664ca6f0002781ef7b223a42137
ooyniz <ooyniz@gmail.com>
2025-06-04 18:59:52 +0900

    feat: 메일 전송, 확인 Controller 추가 (#174)

--- module-api/src/main/java/com/back2basics/domain/user/controller/UserController.java
+++ module-api/src/main/java/com/back2basics/domain/user/controller/UserController.java
@@ -2,19 +2,25 @@ package com.back2basics.domain.user.controller;
 
 import static com.back2basics.domain.user.controller.code.UserResponseCode.USER_CHANGE_PASSWORD_SUCCESS;
 import static com.back2basics.domain.user.controller.code.UserResponseCode.USER_RESET_PASSWORD_SUCCESS;
+import static com.back2basics.domain.user.controller.code.UserResponseCode.USER_SEND_MAIL_SUCCESS;
 
 import com.back2basics.domain.user.dto.request.ChangePasswordRequest;
+import com.back2basics.domain.user.dto.request.ConfirmMailRequest;
+import com.back2basics.domain.user.dto.request.ResetPasswordMailRequest;
 import com.back2basics.domain.user.dto.request.ResetPasswordRequest;
 import com.back2basics.global.response.result.ApiResponse;
 import com.back2basics.security.model.CustomUserDetails;
 import com.back2basics.user.port.in.ChangePasswordUseCase;
+import com.back2basics.user.port.in.ConfirmMailUseCase;
 import com.back2basics.user.port.in.ResetPasswordUseCase;
+import com.back2basics.user.port.in.SendMailUseCase;
 import com.back2basics.user.port.in.command.ChangePasswordCommand;
 import com.back2basics.user.port.in.command.ResetPasswordCommand;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +33,8 @@ public class UserController {
 
     private final ChangePasswordUseCase changePasswordUseCase;
     private final ResetPasswordUseCase resetPasswordUseCase;
+    private final SendMailUseCase sendMailUseCase;
+    private final ConfirmMailUseCase confirmMailUseCase;
 
     @PutMapping("/change-password")
     public ResponseEntity<ApiResponse<Void>> changePassword(
@@ -39,9 +47,20 @@ public class UserController {
         return ApiResponse.success(USER_CHANGE_PASSWORD_SUCCESS);
     }
 
-    // todo: 메일 전송
+    @PostMapping("/send-mail")
+    public ResponseEntity<ApiResponse<Void>> sendMail(
+        @Valid @RequestBody ResetPasswordMailRequest request) {
+        sendMailUseCase.sendResetLink(request.toCommand());
+        return ApiResponse.success(USER_SEND_MAIL_SUCCESS);
+    }
 
-    // 비밀번호 재설정
+    @PostMapping("/reset-password-email")
+    public ResponseEntity<ApiResponse<Void>> resetPasswordEmail(
+        @Valid @RequestBody ConfirmMailRequest request) {
+        confirmMailUseCase.resetPassword(request.toCommand());
+        return ApiResponse.success(USER_CHANGE_PASSWORD_SUCCESS);
+    }
+
     @PutMapping("/reset-password")
     public ResponseEntity<ApiResponse<Void>> resetPassword(
         @Valid @RequestBody ResetPasswordRequest request,
