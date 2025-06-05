532d4c78f6da24c19c24af7e164d4024d03dbfba
ooyniz <ooyniz@gmail.com>
2025-06-05 13:31:34 +0900

    feat: 정보 조회 response 수정 (#220)

--- module-core/src/main/java/com/back2basics/user/service/result/UserInfoResult.java
+++ module-core/src/main/java/com/back2basics/user/service/result/UserInfoResult.java
@@ -5,7 +5,7 @@ import com.back2basics.user.model.UserType;
 import java.time.LocalDateTime;
 
 public record UserInfoResult(Long id, String username, String name, String email, String phone,
-                             String position, UserType userType, Long companyId,
+                             String position, UserType userType, Long companyId, String companyName,
                              LocalDateTime CreatedAt, LocalDateTime updatedAt) {
 
     public static UserInfoResult toResult(User user) {
@@ -18,6 +18,7 @@ public record UserInfoResult(Long id, String username, String name, String email
             user.getPosition(),
             user.getUserType(),
             user.getCompanyId(),
+            null,
             user.getCreatedAt(),
             user.getUpdatedAt()
         );
--- module-core/src/main/java/com/back2basics/user/service/result/UserSimpleResult.java
+++ module-core/src/main/java/com/back2basics/user/service/result/UserSimpleResult.java
@@ -1,5 +1,6 @@
 package com.back2basics.user.service.result;
 
-public record UserSimpleResult(Long id, String username, String name) {
+public record UserSimpleResult(Long id, String username, String name, String phone, String position,
+                               Long companyId, String companyName) {
 
 }
