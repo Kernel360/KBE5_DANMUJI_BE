dc300be87bd3198b1e6cc3d138052a8ed883c06f
ooyniz <ooyniz@gmail.com>
2025-06-05 13:48:32 +0900

    feat: 메일 전송 에러 코드 추가 (#174)

--- module-core/src/main/java/com/back2basics/infra/exception/user/UserErrorCode.java
+++ module-core/src/main/java/com/back2basics/infra/exception/user/UserErrorCode.java
@@ -12,7 +12,8 @@ public enum UserErrorCode implements ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다"),
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "U002", "이미 존재하는 아이디입니다."),
     PASSWORD_MISMATCH(HttpStatus.FORBIDDEN, "U003", "현재 비밀번호가 일치하지 않습니다."),
-    VERIFICATION_FAILED(HttpStatus.FORBIDDEN, "U004", "인증에 실패했습니다.");
+    VERIFICATION_FAILED(HttpStatus.FORBIDDEN, "U004", "인증에 실패했습니다."),
+    MAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "U005", "이메일 전송에 실패했습니다.");
 
     private final HttpStatus status;
     private final String code;
