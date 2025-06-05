fd2c40f001969bfe154f03e844a7c0d563adfdf5
ooyniz <ooyniz@gmail.com>
2025-06-05 11:26:53 +0900

    feat: simpleResult, simpleResponse 추가 (#217)

---
+++ module-api/src/main/java/com/back2basics/domain/user/dto/response/UserSimpleResponse.java
@@ -0,0 +1,4 @@
+package com.back2basics.domain.user.dto.response;
+
+public record UserSimpleResponse() {
+}
---
+++ module-core/src/main/java/com/back2basics/user/service/result/UserSimpleResult.java
@@ -0,0 +1,4 @@
+package com.back2basics.user.service.result;
+
+public record UserSimpleResult() {
+}
