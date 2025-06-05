bade07ab9fac0516c177a51a19b3a9eb60a462a3
ooyniz <ooyniz@gmail.com>
2025-06-06 04:29:00 +0900

    feat: 필드 값 사용을 위해 도메인 객체 주입 (#240)

--- module-core/src/main/java/com/back2basics/projectuser/model/ProjectUser.java
+++ module-core/src/main/java/com/back2basics/projectuser/model/ProjectUser.java
@@ -1,11 +1,10 @@
 package com.back2basics.projectuser.model;
 
+import com.back2basics.company.model.Company;
 import com.back2basics.company.model.CompanyType;
 import com.back2basics.project.model.Project;
-import com.back2basics.project.port.in.command.ProjectCreateCommand;
+import com.back2basics.user.model.User;
 import com.back2basics.user.model.UserType;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,51 +13,52 @@ public class ProjectUser {
 
     // todo: id 말고 domain 으로 해보겠음. mapper 에서 변환시켜주려고.. 아마..
     private final Long id;
-    private final Long projectId;
-    private final Long userId;
-    private Long companyId;
-    private CompanyType companyType;
+    private final Project project;
+    //    private final Long userId;
+    //    private Long companyId;
+    private User user;
+    private Company company;
     private UserType userType;
+    private CompanyType companyType;
 
     @Builder
-    public ProjectUser(Long id, CompanyType companyType, Long projectId, Long userId,
-        Long companyId,
-        UserType userType) {
+    public ProjectUser(Long id, Project project, User user,
+        Company company, UserType userType, CompanyType companyType) {
         this.id = id;
-        this.projectId = projectId;
-        this.userId = userId;
-        this.companyId = companyId;
+        this.project = project;
+        this.user = user;
+        this.company = company;
         this.companyType = companyType;
         this.userType = userType;
     }
 
-    public static ProjectUser create(Long projectId, Long userId, Long companyId,
-        CompanyType companyType, UserType userType) {
+    public static ProjectUser create(Project project, User user,
+        Company company, UserType userType, CompanyType companyType) {
         return ProjectUser.builder()
-            .projectId(projectId)
-            .userId(userId)
-            .companyId(companyId)
-            .companyType(companyType)
+            .project(project)
+            .user(user)
+            .company(company)
             .userType(userType)
+            .companyType(companyType)
             .build();
     }
 
     // todo: 생성 -> 이게 왜 스태틱인지ㅣ 모르겠음 . 스태틱 그냥 모르곘음 , projectUser1 변수명 변경
     // 커멘트에는 List<ProjectUser> 가 있음 .
-    public static List<ProjectUser> createProjectUser(ProjectCreateCommand command,
-        Project project) {
-        List<ProjectUser> projectUsers = new ArrayList<>();
-        for (ProjectUser projectUser : command.getProjectUsers()) {
-            ProjectUser projectUser1 = ProjectUser.builder()
-                .projectId(project.getId())
-                .userId(projectUser.userId)
-                .companyType(projectUser.companyType)
-                .userType(projectUser.userType)
-                .build();
-            projectUsers.add(projectUser1);
-        }
-        return projectUsers;
-    }
+//    public static List<ProjectUser> createProjectUser(ProjectCreateCommand command,
+//        Project project) {
+//        List<ProjectUser> projectUsers = new ArrayList<>();
+//        for (ProjectUser projectUser : command.getProjectUsers()) {
+//            ProjectUser projectUser1 = ProjectUser.builder()
+//                .projectId(project.getId())
+//                .userId(projectUser.userId)
+//                .companyType(projectUser.companyType)
+//                .userType(projectUser.userType)
+//                .build();
+//            projectUsers.add(projectUser1);
+//        }
+//        return projectUsers;
+//    }
 
     // todo: 담당자 변경 (userType 변경)
 
