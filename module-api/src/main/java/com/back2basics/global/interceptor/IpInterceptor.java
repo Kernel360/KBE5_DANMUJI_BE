7579e1804aba32231f797d9fff386809a6f47d89
swlee <swo98@naver.com>
2025-06-06 16:23:46 +0900

    feat: 정확한 ip 값을 가져올 수 있도록 수정 (#244)

--- module-api/src/main/java/com/back2basics/global/interceptor/IpInterceptor.java
+++ module-api/src/main/java/com/back2basics/global/interceptor/IpInterceptor.java
@@ -1,5 +1,6 @@
 package com.back2basics.global.interceptor;
 
+import com.back2basics.global.utils.ClientUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +14,8 @@ public class IpInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) {
-        String ip = getClientIp(request);
+        String ip = ClientUtils.getRemoteIp(request);
         log.info("ip: {}", ip);
         return true;
-    }
-
-    private String getClientIp(HttpServletRequest request) {
-        String forwardedFor = request.getHeader("X-Forwarded-For");
-        return forwardedFor != null ? forwardedFor.split(",")[0] : request.getRemoteAddr();
     }
 }
