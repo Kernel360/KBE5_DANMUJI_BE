8a310cd5748ce61b27899ab939482028525417a6
ooyniz <ooyniz@gmail.com>
2025-06-07 07:25:39 +0900

    refactor: 검증 추가 (#249)

--- module-core/src/main/java/com/back2basics/project/port/in/command/ProjectCreateCommand.java
+++ module-core/src/main/java/com/back2basics/project/port/in/command/ProjectCreateCommand.java
@@ -2,6 +2,7 @@ package com.back2basics.project.port.in.command;
 
 import com.back2basics.projectuser.model.ProjectUser;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
@@ -12,19 +13,19 @@ import lombok.Getter;
 public class ProjectCreateCommand {
 
     @NotBlank(message = "프로젝트명은 필수입니다.")
-    private String name;
-
-    private String description;
-
-    private LocalDate startDate;
-
-    private LocalDate endDate;
-
-    private Long developerId; // 개발사 담당자
-    private Long clientId; // 클라이언트 담당자
-    private Long developCompanyId; // 개발사
-    private Long clientCompanyId; // 클라이언트사
-
+    String name;
+    String description;
+    @NotNull(message = "시작일은 필수입니다.")
+    LocalDate startDate;
+    @NotNull(message = "개발자 아이디는 필수입니다.")
+    Long developerId;
+    @NotNull(message = "클라이언트 아이디는 필수입니다.")
+    Long clientId;
+    @NotNull(message = "개발사 아이디는 필수입니다.")
+    Long developCompanyId;
+    @NotNull(message = "클라이언트사 아이디는 필수입니다.")
+    Long clientCompanyId;
+    LocalDate endDate;
     private List<ProjectUser> projectUsers; // user, companyType, userType 들어감
 }
 
