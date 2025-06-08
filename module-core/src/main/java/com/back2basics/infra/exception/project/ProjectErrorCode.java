4da24989c4657c2a0a377e9c74d008d1222f8334
Jinhy00 <pjhark0@gmail.com>
2025-06-08 15:55:20 +0900

    feat: proejctuser 에러코드 추가(#245)

--- module-core/src/main/java/com/back2basics/infra/exception/project/ProjectErrorCode.java
+++ module-core/src/main/java/com/back2basics/infra/exception/project/ProjectErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ProjectErrorCode implements ErrorCode {
-    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "PJ001", "프로젝트를 찾을 수 없습니다");
+    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "PJ001", "프로젝트를 찾을 수 없습니다"),
+    // todo: project_user error, response code의 위치
+    PROJECT_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "PJU001", "프로젝트 유저를 찾을 수 없습니다");
+
+
 
     private final HttpStatus status;
     private final String code;
