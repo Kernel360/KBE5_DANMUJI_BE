94b0dc9ba8b3ce8cac95e42f1bcd5fbf06dbd0c7
Jinhy00 <pjhark0@gmail.com>
2025-06-08 15:56:47 +0900

    feat: update 필드 추가(#245)

--- module-core/src/main/java/com/back2basics/project/port/in/command/ProjectUpdateCommand.java
+++ module-core/src/main/java/com/back2basics/project/port/in/command/ProjectUpdateCommand.java
@@ -2,16 +2,13 @@ package com.back2basics.project.port.in.command;
 
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 public class ProjectUpdateCommand {
 
-    // todo : custom
     @NotBlank(message = "제목은 공백일 수 없습니다.")
     private String name;
 
@@ -20,4 +17,8 @@ public class ProjectUpdateCommand {
     private LocalDate startDate;
 
     private LocalDate endDate;
+
+    private final Long developerId;
+
+    private final Long clientId;
 }
