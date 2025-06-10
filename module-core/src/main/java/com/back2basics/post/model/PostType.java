1d9fa3b415a41943b712ada2c5a0afddae505265
swlee <swo98@naver.com>
2025-06-10 15:36:44 +0900

    fix: enum타입을 string으로만 보낼 수 있도록 수정 (#270)

--- module-core/src/main/java/com/back2basics/post/model/PostType.java
+++ module-core/src/main/java/com/back2basics/post/model/PostType.java
@@ -1,5 +1,8 @@
 package com.back2basics.post.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum PostType {
     GENERAL,
     NOTICE
