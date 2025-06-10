354aa7ee1f7bf4898adb79e084bee7f4e859eb7c
ooyniz <ooyniz@gmail.com>
2025-06-11 03:46:51 +0900

    feat: JpaRepository Impl (#215)

---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/notification/NotificationEntityRepository.java
@@ -0,0 +1,13 @@
+package com.back2basics.adapter.persistence.notification;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationEntityRepository extends JpaRepository<NotificationEntity, Long> {
+
+    List<NotificationEntity> findByClientId(Long clientId);
+
+    List<NotificationEntity> findByClientIdAndIsReadFalse(Long clientId);
+}
