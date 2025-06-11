39679aab1a9918316db61494c303cc2395e97d44
swlee <swo98@naver.com>
2025-06-12 00:31:12 +0900

    feat: 프로젝트단계 검증클래스 생성 (#308)

--- module-core/src/main/java/com/back2basics/projectstep/port/out/ReadProjectStepPort.java
+++ module-core/src/main/java/com/back2basics/projectstep/port/out/ReadProjectStepPort.java
@@ -2,10 +2,11 @@ package com.back2basics.projectstep.port.out;
 
 import com.back2basics.projectstep.model.ProjectStep;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReadProjectStepPort {
 
-    ProjectStep findById(Long stepId);
+    Optional<ProjectStep> findById(Long stepId);
 
     // todo: 중복 메서드 제거
     List<ProjectStep> findAllByProjectId(Long projectId);
