2b8d1b47850c40c904c45161f66d6ac17a8ef870
ooyniz <ooyniz@gmail.com>
2025-06-12 17:26:21 +0900

    fix: 알림 생성 오류 수정 (#324)

--- module-api/src/main/java/com/back2basics/domain/notify/dto/request/NotificationCreateRequest.java
+++ module-api/src/main/java/com/back2basics/domain/notify/dto/request/NotificationCreateRequest.java
@@ -3,9 +3,10 @@ package com.back2basics.domain.notify.dto.request;
 import com.back2basics.notify.model.NotificationType;
 import com.back2basics.notify.port.in.command.SendNotificationCommand;
 
-public record NotificationCreateRequest(Long referenceId, String message, NotificationType type) {
+public record NotificationCreateRequest(Long clientId, Long referenceId, String message,
+                                        NotificationType type) {
 
-    public SendNotificationCommand toCommand(Long clientId) {
+    public SendNotificationCommand toCommand() {
         return new SendNotificationCommand(clientId, referenceId, message, type);
     }
 }
