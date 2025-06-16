3fd42c38e54ec4c90f06d65ca83123532b7b4ec2
swlee <swo98@naver.com>
2025-06-17 00:17:59 +0900

    fix: 도메인 모델 객체 수정, 빌더 삭제 (#346)

--- module-core/src/main/java/com/back2basics/comment/model/Comment.java
+++ module-core/src/main/java/com/back2basics/comment/model/Comment.java
@@ -1,10 +1,7 @@
 package com.back2basics.comment.model;
 
 import com.back2basics.comment.port.in.command.CommentUpdateCommand;
-import com.back2basics.post.model.Post;
-import com.back2basics.user.model.User;
 import java.time.LocalDateTime;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -15,34 +12,34 @@ public class Comment {
     private Long postId;
     private Long parentCommentId;
     private String authorIp;
-    private User author;
+    private Long authorId;
     private String content;
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    @Builder
-    public Comment(Long id, Long parentId, Long postId, Long parentCommentId, String authorIp,
-        User author,
-        String content,
-        LocalDateTime createdAt, LocalDateTime updatedAt) {
+    private Comment(Long id, Long parentId, Long postId, Long parentCommentId, String authorIp,
+        Long authorId, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.parentId = parentId;
         this.postId = postId;
         this.parentCommentId = parentCommentId;
         this.authorIp = authorIp;
-        this.author = author;
+        this.authorId = authorId;
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
 
+    public static Comment create(Long id, Long parentId, Long postId, Long parentCommentId,
+        String authorIp, Long authorId, String content,
+        LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new Comment(id, parentId, postId, parentCommentId, authorIp, authorId, content,
+            createdAt, updatedAt);
+    }
+
     public void update(CommentUpdateCommand command, String userIp) {
         this.content = command.getContent();
         this.authorIp = userIp;
-    }
-
-    public void assignPostId(Post post) {
-        this.postId = post.getId();
     }
 
 
