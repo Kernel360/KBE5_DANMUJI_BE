6ab5de8858f939acb7be74811946be2df5963f5a
Jinhy00 <pjhark0@gmail.com>
2025-06-18 18:07:16 +0900

    feat: 회원별 프로젝트 목록 조회 response 수정(#356)

--- module-api/src/main/java/com/back2basics/domain/project/dto/response/ProjectListResponse.java
+++ module-api/src/main/java/com/back2basics/domain/project/dto/response/ProjectListResponse.java
@@ -1,6 +1,7 @@
 package com.back2basics.domain.project.dto.response;
 
 import com.back2basics.domain.assignment.dto.response.AssignProjectListResponse;
+import com.back2basics.domain.company.dto.response.CompanySummaryResponse;
 import com.back2basics.project.model.ProjectStatus;
 import com.back2basics.project.service.result.ProjectListResult;
 import java.time.LocalDate;
@@ -8,20 +9,21 @@ import java.util.List;
 
 // todo: 뎁스 하나 더 파서 회사에 멤버 목록으로 나오도록
 public record ProjectListResponse(Long id, String name,
-                                  List<AssignProjectListResponse> assignClients,
-                                  List<AssignProjectListResponse> assignDevs,
+                                  List<CompanySummaryResponse> assignClientCompanies,
+                                  List<CompanySummaryResponse> assignDevCompanies,
                                   LocalDate startDate, LocalDate endDate,
                                   ProjectStatus projectStatus) {
 
 
     public static ProjectListResponse toResponse(ProjectListResult result) {
-        List<AssignProjectListResponse> assignClients = result.assignClients().stream()
-            .map(AssignProjectListResponse::toResponse).toList();
+        List<CompanySummaryResponse> assignClientCompanies = result.assignClientCompanies().stream()
+            .map(CompanySummaryResponse::toResponse).toList();
 
-        List<AssignProjectListResponse> assignDevs = result.assignDevs().stream()
-            .map(AssignProjectListResponse::toResponse).toList();
+        List<CompanySummaryResponse> assignDevCompanies = result.assignDevCompanies().stream()
+            .map(CompanySummaryResponse::toResponse).toList();
 
-        return new ProjectListResponse(result.id(), result.name(), assignClients, assignDevs,
+        return new ProjectListResponse(result.id(), result.name(), assignClientCompanies,
+            assignDevCompanies,
             result.startDate(), result.endDate(), result.projectStatus());
     }
 }
