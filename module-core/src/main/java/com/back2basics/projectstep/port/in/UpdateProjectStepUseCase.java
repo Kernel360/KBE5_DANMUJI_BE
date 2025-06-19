736c89982d8cf2e0a91178a377e3638dc31f3a10
ooyniz <ooyniz@gmail.com>
2025-06-20 04:01:17 +0900

    feat: 프로젝트 단계 순서 변경 - usecase, service 구현 (#386)

--- module-core/src/main/java/com/back2basics/projectstep/port/in/UpdateProjectStepUseCase.java
+++ module-core/src/main/java/com/back2basics/projectstep/port/in/UpdateProjectStepUseCase.java
@@ -2,10 +2,13 @@ package com.back2basics.projectstep.port.in;
 
 import com.back2basics.projectstep.model.ProjectStepStatus;
 import com.back2basics.projectstep.port.in.command.UpdateProjectStepCommand;
+import java.util.List;
 
 public interface UpdateProjectStepUseCase {
 
     void updateStepName(UpdateProjectStepCommand command, Long stepId);
 
     void updateApprovalStatus(ProjectStepStatus projectStepStatus, Long stepId);
+
+    void reorderSteps(Long projectId, List<Long> stepIdsInNewOrder);
 }
--- module-core/src/main/java/com/back2basics/projectstep/service/UpdateProjectStepService.java
+++ module-core/src/main/java/com/back2basics/projectstep/service/UpdateProjectStepService.java
@@ -1,15 +1,23 @@
 package com.back2basics.projectstep.service;
 
+import static com.back2basics.infra.exception.projectstep.ProjectStepErrorCode.STEP_NOT_FOUND;
+
+import com.back2basics.infra.exception.projectstep.ProjectStepException;
 import com.back2basics.projectstep.model.ProjectStep;
 import com.back2basics.projectstep.model.ProjectStepStatus;
 import com.back2basics.projectstep.port.in.UpdateProjectStepUseCase;
 import com.back2basics.projectstep.port.in.command.UpdateProjectStepCommand;
 import com.back2basics.projectstep.port.out.ReadProjectStepPort;
 import com.back2basics.projectstep.port.out.SaveProjectStepPort;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UpdateProjectStepService implements UpdateProjectStepUseCase {
 
@@ -29,5 +37,25 @@ public class UpdateProjectStepService implements UpdateProjectStepUseCase {
         ProjectStep step = readPort.findById(stepId);
         step.updateStatus(projectStepStatus);
         savePort.save(step);
+    }
+
+    @Override
+    public void reorderSteps(Long projectId, List<Long> stepIdsInNewOrder) {
+        List<ProjectStep> steps = readPort.findAllById(stepIdsInNewOrder);
+
+        Map<Long, ProjectStep> stepMap = steps.stream()
+            .collect(Collectors.toMap(ProjectStep::getId, s -> s));
+        for (int i = 0; i < stepIdsInNewOrder.size(); i++) {
+            Long stepId = stepIdsInNewOrder.get(i);
+            ProjectStep step = stepMap.get(stepId);
+
+            if (step == null) {
+                throw new ProjectStepException(STEP_NOT_FOUND);
+            }
+
+            step.updateStepOrder(i + 1);
+        }
+
+        savePort.saveAll(steps);
     }
 }
