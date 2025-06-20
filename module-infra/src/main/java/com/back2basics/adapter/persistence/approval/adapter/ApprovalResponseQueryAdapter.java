e3bb34fc73dba7665513b87ae9a2214a0407df6a
ooyniz <ooyniz@gmail.com>
2025-06-20 17:55:10 +0900

    feat: 쿼리 조회, 단계 변경 저장을 위한 adapter 생성 (#313)

---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/approval/adapter/ApprovalResponseQueryAdapter.java
@@ -0,0 +1,34 @@
+package com.back2basics.adapter.persistence.approval.adapter;
+
+import static com.back2basics.infra.exception.approval.ApprovalErrorCode.APPROVAL_NOT_FOUND;
+
+import com.back2basics.adapter.persistence.approval.mapper.ApprovalResponseMapper;
+import com.back2basics.adapter.persistence.approval.repository.ApprovalResponseEntityRepository;
+import com.back2basics.approval.model.ApprovalResponse;
+import com.back2basics.approval.port.out.ApprovalResponseQueryPort;
+import com.back2basics.infra.exception.approval.ApprovalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class ApprovalResponseQueryAdapter implements ApprovalResponseQueryPort {
+
+    private final ApprovalResponseMapper mapper;
+    private final ApprovalResponseEntityRepository approvalResponseEntityRepository;
+
+    @Override
+    public boolean existsByIdAndUserId(Long responseId, Long approverId) {
+        return approvalResponseEntityRepository.existsByIdAndApproverId(responseId,
+            approverId);
+    }
+
+    @Override
+    public ApprovalResponse findById(Long responseId) {
+        return approvalResponseEntityRepository.findById(responseId)
+            .map(mapper::toDomain)
+            .orElseThrow(() -> new ApprovalException(APPROVAL_NOT_FOUND));
+    }
+}
