6bd97d8541588661349770856029acd14435d445
ooyniz <ooyniz@gmail.com>
2025-06-20 18:09:31 +0900

    feat: 상태 변경을 위한 API 추가 (#313)

--- module-api/src/main/java/com/back2basics/domain/approval/controller/ApprovalController.java
+++ module-api/src/main/java/com/back2basics/domain/approval/controller/ApprovalController.java
@@ -1,8 +1,11 @@
 package com.back2basics.domain.approval.controller;
 
 import static com.back2basics.domain.approval.controller.code.ApprovalResponseCode.APPROVAL_REQUEST_CREATE_SUCCESS;
+import static com.back2basics.domain.approval.controller.code.ApprovalResponseCode.APPROVAL_REQUEST_UPDATE_SUCCESS;
 
 import com.back2basics.approval.port.in.CreateApprovalRequestUseCase;
+import com.back2basics.approval.port.in.UpdateApprovalResponseUseCase;
+import com.back2basics.domain.approval.dto.request.UpdateApprovalRequest;
 import com.back2basics.global.response.result.ApiResponse;
 import com.back2basics.security.model.CustomUserDetails;
 import java.util.List;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +25,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApprovalController {
 
     private final CreateApprovalRequestUseCase createApprovalRequestUseCase;
+    private final UpdateApprovalResponseUseCase updateApprovalResponseUseCase;
 
     @PostMapping("/{stepId}")
     public ResponseEntity<ApiResponse<Void>> create(@PathVariable Long stepId,
         @AuthenticationPrincipal
-        CustomUserDetails userDetails, @RequestBody List<Long> approverIds) {
+        CustomUserDetails userDetails, @RequestBody List<Long> responseIds) {
 
         createApprovalRequestUseCase.create(stepId, userDetails.getId(),
-            approverIds);
+            responseIds);
         return ApiResponse.success(APPROVAL_REQUEST_CREATE_SUCCESS);
+    }
+
+    @PutMapping("/{responseId}/status")
+    public ResponseEntity<ApiResponse<Void>> change(@PathVariable Long responseId,
+        @AuthenticationPrincipal CustomUserDetails userDetails,
+        @RequestBody UpdateApprovalRequest request) {
+        updateApprovalResponseUseCase.change(responseId, userDetails.getId(), request.toCommand());
+        return ApiResponse.success(APPROVAL_REQUEST_UPDATE_SUCCESS);
     }
 }
