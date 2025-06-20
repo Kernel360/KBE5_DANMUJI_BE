d1f0c9f8b99377ff64e49231a768153c49d630a5
ooyniz <ooyniz@gmail.com>
2025-06-20 15:47:14 +0900

    feat: 저장을 위한 port 생성 및 adapter 구현 - 쿼리 향상을 위해 getReferenceById 메서드 사용 (#313)

---
+++ module-core/src/main/java/com/back2basics/approval/port/out/ApprovalRequestCommandPort.java
@@ -0,0 +1,8 @@
+package com.back2basics.approval.port.out;
+
+import com.back2basics.approval.model.ApprovalRequest;
+
+public interface ApprovalRequestCommandPort {
+
+    void save(ApprovalRequest approvalRequest);
+}
