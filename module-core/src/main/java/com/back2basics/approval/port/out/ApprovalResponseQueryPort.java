8d2bebd83d5bd3c549ec96be5ca34505a290136c
ooyniz <ooyniz@gmail.com>
2025-06-21 18:28:34 +0900

    feat: 승인 요청에 대한 승인자 목록 조회를 위한 도메인 포트 정의 (#373)

--- module-core/src/main/java/com/back2basics/approval/port/out/ApprovalResponseQueryPort.java
+++ module-core/src/main/java/com/back2basics/approval/port/out/ApprovalResponseQueryPort.java
@@ -1,10 +1,13 @@
 package com.back2basics.approval.port.out;
 
 import com.back2basics.approval.model.ApprovalResponse;
+import java.util.List;
 
 public interface ApprovalResponseQueryPort {
 
     boolean existsByIdAndUserId(Long responseId, Long userId);
 
     ApprovalResponse findById(Long responseId);
+
+    List<Long> findApproverIdsByRequestId(Long stepId);
 }
