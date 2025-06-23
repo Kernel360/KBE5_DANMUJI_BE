2f7e5530a65acabf930cce50e25a11a0e42795c9
Jinhy00 <pjhark0@gmail.com>
2025-06-23 16:17:07 +0900

    refactor: mapper 수정 (#355)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/projectstep/ProjectStepMapper.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/projectstep/ProjectStepMapper.java
@@ -3,12 +3,10 @@ package com.back2basics.adapter.persistence.projectstep;
 import static com.back2basics.infra.exception.project.ProjectErrorCode.PROJECT_NOT_FOUND;
 
 import com.back2basics.adapter.persistence.project.ProjectEntity;
+import com.back2basics.adapter.persistence.project.ProjectEntityRepository;
 import com.back2basics.adapter.persistence.project.ProjectMapper;
-import com.back2basics.adapter.persistence.user.entity.UserEntity;
-import com.back2basics.adapter.persistence.user.mapper.UserMapper;
 import com.back2basics.infra.exception.project.ProjectException;
 import com.back2basics.project.model.Project;
-import com.back2basics.project.port.out.ReadProjectPort;
 import com.back2basics.projectstep.model.ProjectStep;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
@@ -17,12 +15,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ProjectStepMapper {
-    // todo: project를 여기서 찾아서 넣어줘도 되는지 멘토링 때 질문, 연관관계를 아직 끊짐 못헀음
+    // todo: project를 여기서 찾아서 넣어줘도 되는지 멘토링 때 질문
 
     @Lazy
     private final ProjectMapper projectMapper;
 
-    private final ReadProjectPort readProjectPort;
+    private final ProjectEntityRepository projectEntityRepository;
 
     public ProjectStep toDomain(ProjectStepEntity entity) {
         return ProjectStep.builder()
@@ -36,36 +34,12 @@ public class ProjectStepMapper {
             .build();
     }
 
-    public ProjectStep toDomainTest(ProjectStepEntity entity) {
-        return ProjectStep.builder()
-            .stepId(entity.getStepId())
-            .projectId(entity.getProject().getId())
-            .name(entity.getName())
-            .stepOrder(entity.getStepOrder())
-            .projectStepStatus(entity.getProjectStepStatus())
-            .isDeleted(entity.isDeleted())
-            .deletedAt(entity.getDeletedAt())
-            .build();
-    }
-
-    public ProjectStepEntity toEntity(ProjectStep projectStep, ProjectEntity project) {
-        return ProjectStepEntity.builder()
-            .stepId(projectStep.getStepId())
-            .project(project)
-            .name(projectStep.getName())
-            .stepOrder(projectStep.getStepOrder())
-            .projectStepStatus(projectStep.getProjectStepStatus())
-            .isDeleted(projectStep.isDeleted())
-            .deletedAt(projectStep.getDeletedAt())
-            .build();
-    }
-
-    public ProjectStepEntity toCreateEntity(ProjectStep projectStep) {
-        Project project = readProjectPort.findById(projectStep.getProjectId())
+    public ProjectStepEntity toEntity(ProjectStep projectStep) {
+        ProjectEntity projectEntity = projectEntityRepository.findById(projectStep.getProjectId())
             .orElseThrow(() -> new ProjectException(PROJECT_NOT_FOUND));
         return ProjectStepEntity.builder()
             .stepId(projectStep.getStepId())
-            .project(projectMapper.toCreateEntity(project))
+            .project(projectEntity)
             .name(projectStep.getName())
             .stepOrder(projectStep.getStepOrder())
             .projectStepStatus(projectStep.getProjectStepStatus())
