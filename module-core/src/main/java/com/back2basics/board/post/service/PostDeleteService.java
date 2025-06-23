3a897e2ddad052c9a3a78b79503bbce541243878
swlee <swo98@naver.com>
2025-06-23 15:40:12 +0900

    feat: 게시그르 삭제 시 history 추가 (#399)

--- module-core/src/main/java/com/back2basics/board/post/service/PostDeleteService.java
+++ module-core/src/main/java/com/back2basics/board/post/service/PostDeleteService.java
@@ -1,9 +1,13 @@
 package com.back2basics.board.post.service;
 
-import com.back2basics.infra.validation.validator.PostValidator;
 import com.back2basics.board.post.model.Post;
 import com.back2basics.board.post.port.in.PostDeleteUseCase;
 import com.back2basics.board.post.port.out.PostSoftDeletePort;
+import com.back2basics.history.model.History;
+import com.back2basics.history.port.out.HistoryCreatePort;
+import com.back2basics.infra.validation.validator.PostValidator;
+import java.time.LocalDateTime;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,7 @@ public class PostDeleteService implements PostDeleteUseCase {
 
     private final PostSoftDeletePort postSoftDeletePort;
     private final PostValidator postValidator;
+    private final HistoryCreatePort historyCreatePort;
 
     @Override
     public void softDeletePost(Long requesterId, Long postId) {
@@ -21,6 +26,20 @@ public class PostDeleteService implements PostDeleteUseCase {
 
         post.markDeleted();
 
+        History history = History.create(
+            "post",
+            post.getId(),
+            String.valueOf(post.getAuthorId()),
+            Map.of(
+                "before", post
+            ),
+            Map.of(
+                "isDeleted", post.isDelete()
+            ),
+            LocalDateTime.now()
+        );
+
+        historyCreatePort.save(history);
         postSoftDeletePort.softDelete(post);
     }
 }
