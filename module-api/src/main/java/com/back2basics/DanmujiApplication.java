2b55fff461b89727b3a9cc200bb55e5db20e9239
swlee <swo98@naver.com>
2025-06-23 12:19:23 +0900

    feat: mongodb 사용을 위한 configuration 추가, main.app에 scan range 등록 (#399)

--- module-api/src/main/java/com/back2basics/DanmujiApplication.java
+++ module-api/src/main/java/com/back2basics/DanmujiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.back2basics")
 @EntityScan("com.back2basics")
 @EnableJpaRepositories(basePackages = "com.back2basics")
+@EnableMongoRepositories(basePackages = "com.back2basics")
 public class DanmujiApplication {
 
     public static void main(String[] args) {
