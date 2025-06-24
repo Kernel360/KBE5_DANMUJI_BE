5c48044c9be6367fe65b9ce7fbc230f3c80725f9
Jinhy00 <pjhark0@gmail.com>
2025-06-24 21:23:37 +0900

    feat: findType repository 작성(#431)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/assignment/AssignmentEntityRepository.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/assignment/AssignmentEntityRepository.java
@@ -12,15 +12,24 @@ public interface AssignmentEntityRepository extends JpaRepository<AssignmentEnti
 
     List<AssignmentEntity> findByProject_Id(Long projectId);
 
-    AssignmentEntity findByProjectIdAndUserTypeAndCompanyType(Long projectId, UserType userType,
-        CompanyType companyType);
-
-    List<AssignmentEntity> findByProject_IdAndUser_Id(Long projectId, Long userId);
-
-    List<AssignmentEntity> findByUserIdAndProjectIsDeletedFalse(Long userId);
-
     List<AssignmentEntity> findByProject_IdAndCompany_Id(Long projectId, Long companyId);
 
     @Query("SELECT a.user.id FROM AssignmentEntity a WHERE a.project.id = :projectId")
     List<Long> findUserIdsByProjectId(Long projectId);
+
+    @Query("""
+    SELECT a.companyType
+    FROM AssignmentEntity a
+    WHERE a.project.id = :projectId
+      AND a.user.id = :userId
+""")
+    CompanyType findCompanyTypeByProject_IdAndUser_Id(Long projectId, Long userId);
+
+    @Query("""
+    SELECT a.userType
+    FROM AssignmentEntity a
+    WHERE a.project.id = :projectId
+      AND a.user.id = :userId
+""")
+    UserType findUserTypeByProject_IdAndUser_Id(Long projectId, Long userId);
 }
