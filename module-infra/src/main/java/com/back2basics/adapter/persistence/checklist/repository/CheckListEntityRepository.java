e19b95bd5acb3bec37e04570056052aa2cb799ee
ooyniz <ooyniz@gmail.com>
2025-06-24 14:22:02 +0900

    feat: entityRepository 생성 (#415)

---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/checklist/repository/CheckListEntityRepository.java
@@ -0,0 +1,10 @@
+package com.back2basics.adapter.persistence.checklist.repository;
+
+import com.back2basics.adapter.persistence.checklist.entity.CheckListEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckListEntityRepository extends JpaRepository<CheckListEntity, Long> {
+
+}
