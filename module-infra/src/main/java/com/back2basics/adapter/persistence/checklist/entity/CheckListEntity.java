2da75cb9c2e2d3e298cb37e0726ed08a1cc1e703
ooyniz <ooyniz@gmail.com>
2025-06-24 12:05:40 +0900

    feat: checkList 엔티티 생성 (#415)

---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/checklist/entity/CheckListEntity.java
@@ -0,0 +1,59 @@
+package com.back2basics.adapter.persistence.checklist.entity;
+
+import com.back2basics.adapter.persistence.board.post.PostEntity;
+import com.back2basics.adapter.persistence.user.entity.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "checklists")
+public class CheckListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @Column(name = "checked_at")
+    private LocalDateTime checkedAt;
+
+    @Column(name = "is_checked", nullable = false)
+    private Boolean isChecked;
+
+    @Column(name = "deletedAt")
+    private LocalDateTime deletedAt;
+
+    public CheckListEntity(Long id, String content, UserEntity user, PostEntity post,
+        LocalDateTime checkedAt, Boolean isChecked, LocalDateTime deletedAt) {
+        this.id = id;
+        this.content = content;
+        this.user = user;
+        this.post = post;
+        this.checkedAt = checkedAt;
+        this.isChecked = isChecked;
+        this.deletedAt = deletedAt;
+    }
+}
