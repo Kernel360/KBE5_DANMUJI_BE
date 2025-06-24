62d1aaf58883c96bc32e0e952b6cfada2baf3efb
ooyniz <ooyniz@gmail.com>
2025-06-24 14:38:17 +0900

    feat: 체크리스트 수정을 위한 usecase, service 구현 (#415)

---
+++ module-core/src/main/java/com/back2basics/checklist/port/in/UpdateCheckListUseCase.java
@@ -0,0 +1,8 @@
+package com.back2basics.checklist.port.in;
+
+import com.back2basics.checklist.port.in.command.CreateCheckListCommand;
+
+public interface UpdateCheckListUseCase {
+
+    void update(Long checkListId, CreateCheckListCommand command);
+}
---
+++ module-core/src/main/java/com/back2basics/checklist/service/UpdateCheckListService.java
@@ -0,0 +1,24 @@
+package com.back2basics.checklist.service;
+
+import com.back2basics.checklist.model.CheckList;
+import com.back2basics.checklist.port.in.UpdateCheckListUseCase;
+import com.back2basics.checklist.port.in.command.CreateCheckListCommand;
+import com.back2basics.checklist.port.out.CheckListCommandPort;
+import com.back2basics.checklist.port.out.CheckListQueryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateCheckListService implements UpdateCheckListUseCase {
+
+    private final CheckListCommandPort checkListCommandPort;
+    private final CheckListQueryPort checkListQueryPort;
+
+    @Override
+    public void update(Long checkListId, CreateCheckListCommand command) {
+        CheckList checkList = checkListQueryPort.findById(checkListId);
+        checkList.update(command.content());
+        checkListCommandPort.save(checkList);
+    }
+}
