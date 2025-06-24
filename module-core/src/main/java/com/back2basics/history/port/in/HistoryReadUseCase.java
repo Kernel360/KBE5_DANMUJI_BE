b70e468a9f167afce3c29995595b0006b4757d36
swlee <swo98@naver.com>
2025-06-24 14:39:07 +0900

    feat: 히스토리 컨트롤러 구현 (#419)

--- module-core/src/main/java/com/back2basics/history/port/in/HistoryReadUseCase.java
+++ module-core/src/main/java/com/back2basics/history/port/in/HistoryReadUseCase.java
@@ -1,13 +1,15 @@
 package com.back2basics.history.port.in;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface HistoryReadUseCase {
 
-    HistoryResult getHistory(Long historyId);
+    HistoryResult getHistoryById(Long userId, Long historyId);
 
-    Page<HistorySimpleResult> getHistories();
+    Page<HistorySimpleResult> getAllHistories(Long userId, Pageable pageable);
 
-    Page<HistorySimpleResult> searchHistories();
+    Page<HistorySimpleResult> searchHistories(Long userId, HistorySearchCommand command,
+        Pageable pageable);
 
 }
