6445633e4f0184fe19625e33175aaa05113e09a1
Jinhy00 <pjhark0@gmail.com>
2025-06-24 21:22:52 +0900

    feat: response 필드 추가(#431)

--- module-api/src/main/java/com/back2basics/domain/project/dto/response/ProjectDetailResponse.java
+++ module-api/src/main/java/com/back2basics/domain/project/dto/response/ProjectDetailResponse.java
@@ -1,15 +1,17 @@
 package com.back2basics.domain.project.dto.response;
 
+import com.back2basics.company.model.CompanyType;
 import com.back2basics.domain.assignment.dto.response.AssignProjectListResponse;
 import com.back2basics.domain.projectstep.dto.response.ProjectStepResponse;
 import com.back2basics.project.model.ProjectStatus;
 import com.back2basics.project.service.result.ProjectDetailResult;
+import com.back2basics.user.model.UserType;
 import java.time.LocalDate;
 import java.util.List;
 
 public record ProjectDetailResponse(Long id, String name, String description, LocalDate startDate,
                                     LocalDate endDate, ProjectStatus projectStatus,
-                                    int progress,
+                                    int progress, UserType userType,  CompanyType myProjectRole,
                                     List<AssignProjectListResponse> clients,
                                     List<AssignProjectListResponse> developers,
                                     List<ProjectStepResponse> steps) {
@@ -23,6 +25,8 @@ public record ProjectDetailResponse(Long id, String name, String description, Lo
             result.endDate(),
             result.projectStatus(),
             result.progress(),
+            result.userType(),
+            result.myProjectRole(),
             result.clients().stream()
                 .map(AssignProjectListResponse::toResponse)
                 .toList(),
