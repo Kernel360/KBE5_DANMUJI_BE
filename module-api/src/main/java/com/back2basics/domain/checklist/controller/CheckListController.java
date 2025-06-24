0c8640b16af67f957554d7b62abd24d165f594a5
ooyniz <ooyniz@gmail.com>
2025-06-24 14:39:13 +0900

    feat: 체크리스트 수정 API 추가 (#415)

--- module-api/src/main/java/com/back2basics/domain/checklist/controller/CheckListController.java
+++ module-api/src/main/java/com/back2basics/domain/checklist/controller/CheckListController.java
@@ -1,15 +1,19 @@
 package com.back2basics.domain.checklist.controller;
 
 import static com.back2basics.domain.checklist.controller.code.CheckListResponseCode.CHECK_LIST_CREATE_SUCCESS;
+import static com.back2basics.domain.checklist.controller.code.CheckListResponseCode.CHECK_LIST_UPDATE_SUCCESS;
 
 import com.back2basics.checklist.port.in.CreateCheckListUseCase;
+import com.back2basics.checklist.port.in.UpdateCheckListUseCase;
 import com.back2basics.domain.checklist.dto.request.CreateCheckListRequest;
 import com.back2basics.global.response.result.ApiResponse;
 import com.back2basics.security.model.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckListController {
 
     private final CreateCheckListUseCase createCheckListUseCase;
+    private final UpdateCheckListUseCase updateCheckListUseCase;
 
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> create(@RequestParam("postId") Long postId,
@@ -28,6 +33,13 @@ public class CheckListController {
         CustomUserDetails userDetails, @RequestBody CreateCheckListRequest request) {
         createCheckListUseCase.create(userDetails.getId(), postId, request.toCommand());
         return ApiResponse.success(CHECK_LIST_CREATE_SUCCESS);
+    }
+
+    @PutMapping("/{checkListId}")
+    public ResponseEntity<ApiResponse<Void>> update(@PathVariable Long checkListId,
+        @RequestBody CreateCheckListRequest request) {
+        updateCheckListUseCase.update(checkListId, request.toCommand());
+        return ApiResponse.success(CHECK_LIST_UPDATE_SUCCESS);
     }
 
 }
