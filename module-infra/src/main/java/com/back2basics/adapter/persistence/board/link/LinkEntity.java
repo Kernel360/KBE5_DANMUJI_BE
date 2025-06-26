db8b122046199d6ab40feab7ef241ab7653d8e0d
swlee <swo98@naver.com>
2025-06-26 14:33:35 +0900

    feat: 링크 첨부 추가를 위한 엔티티,모델 객체 생성 (#452)

---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/board/link/LinkEntity.java
@@ -0,0 +1,44 @@
+package com.back2basics.adapter.persistence.board.link;
+
+
+import com.back2basics.board.link.model.Link;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "links")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LinkEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "url")
+    private String url;
+
+    public LinkEntity(Long id, Long postId, String url) {
+        this.id = id;
+        this.postId = postId;
+        this.url = url;
+    }
+
+    public static LinkEntity of(Link link) {
+        return new LinkEntity(
+            link.getId(),
+            link.getId(),
+            link.getUrl()
+        );
+    }
+}
---
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/board/link/LinkEntityRepository.java
@@ -0,0 +1,7 @@
+package com.back2basics.adapter.persistence.board.link;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkEntityRepository extends JpaRepository<LinkEntity, Long> {
+
+}
