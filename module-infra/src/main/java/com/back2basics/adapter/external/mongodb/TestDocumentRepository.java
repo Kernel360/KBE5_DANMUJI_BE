46c4442b777e8a6a7ea66320958f0bb02a6bdaaf
swlee <swo98@naver.com>
2025-06-26 14:28:20 +0900

    files: mongodb 테스트를 위해 사용했던 클래스들 삭제 (#452)

--- module-infra/src/main/java/com/back2basics/adapter/external/mongodb/TestDocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.back2basics.adapter.external.mongodb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestDocumentRepository extends MongoRepository<TestDocument, String> {
-
-}
