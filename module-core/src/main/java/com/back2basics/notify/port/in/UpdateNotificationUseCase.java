611de66fd65e763a78d547507abdcda9d434c105
ooyniz <ooyniz@gmail.com>
2025-07-01 17:02:17 +0900

    refactor: 읽기 전용 -> 토글 읽기 로직 수정 (#491)

--- module-core/src/main/java/com/back2basics/notify/port/in/UpdateNotificationUseCase.java
+++ module-core/src/main/java/com/back2basics/notify/port/in/UpdateNotificationUseCase.java
@@ -2,7 +2,7 @@ package com.back2basics.notify.port.in;
 
 public interface UpdateNotificationUseCase {
 
-    void markAsRead(Long notificationId);
+    void toggleRead(Long notificationId);
 
     void markAllAsRead(Long clientId);
 
