735b03056f5378e84f646e78e6bfdf82c531a2b0
ooyniz <ooyniz@gmail.com>
2025-07-03 14:30:59 +0900

    feat: 체크리스트 생성 시 알림 (#444)

--- module-core/src/main/java/com/back2basics/checklist/service/CreateChecklistService.java
+++ module-core/src/main/java/com/back2basics/checklist/service/CreateChecklistService.java
@@ -6,9 +6,12 @@ import com.back2basics.checklist.port.in.command.CreateChecklistCommand;
 import com.back2basics.checklist.port.out.ChecklistCommandPort;
 import com.back2basics.history.model.DomainType;
 import com.back2basics.history.service.HistoryLogService;
-import com.back2basics.infra.validation.validator.ProjectStepValidator;
 import com.back2basics.infra.validation.validator.UserValidator;
+import com.back2basics.notify.model.NotificationType;
 import com.back2basics.notify.port.in.NotifyUseCase;
+import com.back2basics.notify.port.in.command.SendNotificationCommand;
+import com.back2basics.projectstep.model.ProjectStep;
+import com.back2basics.projectstep.port.out.ReadProjectStepPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +20,15 @@ import org.springframework.stereotype.Service;
 public class CreateChecklistService implements CreateChecklistUseCase {
 
     private final UserValidator userValidator; // validation 처리는 service에서 처리해야 함
-    private final ProjectStepValidator projectStepValidator;
+    private final ReadProjectStepPort readProjectStepPort;
     private final ChecklistCommandPort checklistCommandPort;
     private final NotifyUseCase notifyUseCase;
     private final HistoryLogService historyLogService;
 
     @Override
     public void create(Long stepId, Long userId, CreateChecklistCommand command) {
-        projectStepValidator.validateNotFoundStepId(stepId);
+        ProjectStep projectStep = readProjectStepPort.findById(stepId);
+        Long projectId = projectStep.getProjectId();
         userValidator.validateNotFoundUserId(userId);
         userValidator.validateAllUsersExist(command.responseIds());
 
@@ -33,15 +37,16 @@ public class CreateChecklistService implements CreateChecklistUseCase {
         Checklist savedChecklist = checklistCommandPort.create(checklist);
 
         // todo
-//        for (Long clientId : command.responseIds()) {
-//            SendNotificationCommand notifyCommand = new SendNotificationCommand(
-//                clientId,
-//                savedApprovalRequest.getId(),
-//                NotificationType.STEP_APPROVAL_REQUEST.getDescription(),
-//                NotificationType.STEP_APPROVAL_REQUEST
-//            );
-//            notifyUseCase.notify(notifyCommand);
-//        }
+        for (Long clientId : command.responseIds()) {
+            SendNotificationCommand notifyCommand = new SendNotificationCommand(
+                clientId,
+                projectId,
+                savedChecklist.getId(),
+                NotificationType.CHECKLIST_REQUEST.getDescription(),
+                NotificationType.CHECKLIST_REQUEST
+            );
+            notifyUseCase.notify(notifyCommand);
+        }
 
         historyLogService.logCreated(DomainType.CHECKLIST, userId, savedChecklist,
             "체크리스트 생성");
