5e2d07d5886db854633a8e1f50106f1487519406
swlee <swo98@naver.com>
2025-07-03 17:04:01 +0900

    fix: /api/** 를 allowURL에서 제외하여 모든 api가 인증/인가를 통과하는 문제를 해결 (#525)

--- module-api/src/main/java/com/back2basics/global/security/config/SecurityConfig.java
+++ module-api/src/main/java/com/back2basics/global/security/config/SecurityConfig.java
@@ -28,7 +28,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final String[] allowedUrls = {"/", "/api/auth/**", "/error", "/api/**", "/mongo/**",
+    private final String[] allowedUrls = {"/", "/api/auth/**", "/error", "/mongo/**",
         "/api/users/password/reset-mail/request", "/api/users/password/reset-mail/confirm"};
     private final String[] swaggerUrls = {"/danmuji-ui.html", "/v3/api-docs/**", "/swagger-ui/**",
         "/swagger-ui.html", "/webjars/**", "/favicon.ico"};
