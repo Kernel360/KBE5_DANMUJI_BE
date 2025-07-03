866feca6f85c4766f05563c06da0fa8cb65cad81
swlee <swo98@naver.com>
2025-07-03 12:25:04 +0900

    fix: 게시글 삭제 시 관리자의 경우 author 검증 안하도록 수정

--- module-core/src/main/java/com/back2basics/board/post/service/PostDeleteService.java
+++ module-core/src/main/java/com/back2basics/board/post/service/PostDeleteService.java
@@ -6,6 +6,7 @@ import com.back2basics.board.post.port.out.PostSoftDeletePort;
 import com.back2basics.history.model.DomainType;
 import com.back2basics.history.service.HistoryLogService;
 import com.back2basics.infra.validation.validator.PostValidator;
+import com.back2basics.infra.validation.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +17,14 @@ public class PostDeleteService implements PostDeleteUseCase {
     private final PostSoftDeletePort postSoftDeletePort;
     private final PostValidator postValidator;
     private final HistoryLogService historyLogService;
+    private final UserValidator userValidator;
 
     @Override
     public void softDeletePost(Long requesterId, Long postId) {
         Post post = postValidator.findPost(postId);
-        postValidator.isAuthor(post, requesterId);
+        if(!userValidator.isAdmin(requesterId)){
+            postValidator.isAuthor(post, requesterId);
+        }
 
         post.markDeleted();
 
