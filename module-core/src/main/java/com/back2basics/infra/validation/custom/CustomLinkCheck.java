dedf929e3efc86f95b84d61595c71bafc74b9e68
swlee <swo98@naver.com>
2025-07-04 11:38:39 +0900

    feat: 유효한 url 입력을 받기 위한 커스텀 어노테이션 추가

---
+++ module-core/src/main/java/com/back2basics/infra/validation/custom/CustomLinkCheck.java
@@ -0,0 +1,17 @@
+package com.back2basics.infra.validation.custom;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CustomEnumCheckValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomLinkCheck {
+    String message() default "링크 목록에 중복이 있습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ module-core/src/main/java/com/back2basics/infra/validation/custom/CustomLinkCheckValidator.java
@@ -0,0 +1,16 @@
+package com.back2basics.infra.validation.custom;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class CustomLinkCheckValidator implements ConstraintValidator<CustomLinkCheck, List<String>> {
+
+    @Override
+    public boolean isValid(List<String> value, ConstraintValidatorContext context) {
+        if (value == null) return true;
+        return value.stream().allMatch(link ->
+            link.startsWith("http://") || link.startsWith("https://")
+        );
+    }
+}
