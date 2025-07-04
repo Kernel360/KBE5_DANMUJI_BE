c8c2c9225a7417e7b0cfaa98728006ec8249343c
Jinhy00 <pjhark0@gmail.com>
2025-07-04 15:35:16 +0900

    refactor : 주석 삭제 및 추가 (#527)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/user/mapper/UserMapper.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/user/mapper/UserMapper.java
@@ -45,29 +45,11 @@ public class UserMapper {
             .lastLoginAt(user.getLastLoginAt())
             .build();
     }
-
-    // todo : adapter에서는 자기자신의 도메인 객체만 mpper로 전달해주고 mapper에서 연관 넣어줄지
-    // todo : adapter에서 연관 객체 찾아서 mapper로는 자기자신의 도메인 객체를 그냥 전달만할지
+    
     public UserEntity toEntity(User user) {
         CompanyEntity companyEntity = user.getCompanyId() != null
             ? CompanyEntity.builder().id(user.getCompanyId()).build()
             : null;
         return toEntity(user, companyEntity);
     }
-
-    public UserEntity toEntityTest(User user) {
-        return UserEntity.builder()
-            .id(user.getId())
-            .username(user.getUsername())
-            .password(user.getPassword())
-            .name(user.getName())
-            .email(user.getEmail())
-            .phone(user.getPhone())
-            .position(user.getPosition())
-            .role(user.getRole())
-            .company(companyMapper.toEntity(user.getCompany()))
-            .lastLoginAt(user.getLastLoginAt())
-            .build();
-    }
-
 }
