139b0544a9406e2a5f1043cf85a8cbbb4aa0ffea
Jinhy00 <pjhark0@gmail.com>
2025-07-04 15:36:26 +0900

    refactor : 프로젝트 생성 로직 도메인에 위치 (#527)

--- module-core/src/main/java/com/back2basics/project/service/CreateProjectService.java
+++ module-core/src/main/java/com/back2basics/project/service/CreateProjectService.java
@@ -4,9 +4,7 @@ import com.back2basics.assignment.model.Assignment;
 import com.back2basics.assignment.service.notification.AssignmentNotificationSender;
 import com.back2basics.history.model.DomainType;
 import com.back2basics.history.service.HistoryLogService;
-import com.back2basics.infra.validation.validator.ProjectValidator;
 import com.back2basics.project.model.Project;
-import com.back2basics.project.model.ProjectStatus;
 import com.back2basics.project.port.in.CreateProjectUseCase;
 import com.back2basics.project.port.in.command.ProjectCreateCommand;
 import com.back2basics.project.port.out.SaveProjectPort;
@@ -30,28 +28,19 @@ public class CreateProjectService implements CreateProjectUseCase {
     private final SaveProjectUserPort saveProjectUserPort;
     private final UserQueryPort userQueryPort;
     private final AssignmentNotificationSender assignmentNotificationSender;
-    private final ProjectValidator projectValidator;
     private final HistoryLogService historyLogService;
 
     private static final List<String> DEFAULT_STEPS =
-        List.of("요구사항 정의", "화면설계", "디자인", "퍼블리싱", "개발", "검수");
+        List.of("계약", "요구사항 정의", "화면설계", "개발", "검수", "수금");
 
     @Override
     @Transactional
     public void createProject(ProjectCreateCommand command, Long loggedInUserId) {
-        Project project = Project.builder()
-            .name(command.getName())
-            .description(command.getDescription())
-            .projectCost(command.getProjectCost())
-            .startDate(command.getStartDate())
-            .endDate(command.getEndDate())
-            .projectStatus(ProjectStatus.IN_PROGRESS)
-            .build();
+        Project project = Project.create(command);
         Project savedProject = saveProjectPort.createSave(project);
         historyLogService.logCreated(DomainType.PROJECT, loggedInUserId, savedProject,
             "프로젝트 신규 등록");
 
-        projectValidator.findById(savedProject.getId());
         createDefaultSteps(savedProject.getId());
         assignUsers(savedProject, command);
     }
@@ -70,10 +59,6 @@ public class CreateProjectService implements CreateProjectUseCase {
             );
             saveProjectStepPort.save(step);
         }
-
-        // todo : 주석 이유 : saveProjectStepPort.saveAll(steps); 이후 리턴 값으로 saved객체를 리턴으로 받아야
-        // id값이 생기는데, saveAll() 치고나면 void라서 지금 아래 'assignments' 객체 안에는 id가 없음 - >에러
-        //historyLogService.logCreated(DomainType.STEP, loggedInUserId, steps, "기본 단계 등록");
     }
 
 
@@ -94,9 +79,5 @@ public class CreateProjectService implements CreateProjectUseCase {
         List<Long> assignmentIds = assignments.stream()
             .map(assignment -> assignment.getUser().getId()).toList();
         assignmentNotificationSender.sendNotification(assignmentIds, project.getId());
-
-        // todo : 주석 이유 : saveProjectUserPort.saveAll(assignments)  이후 리턴 값으로 saved객체를 리턴으로 받아야
-        // id값이 생기는데, saveAll() 치고나면 void라서 지금 아래 'assignments' 객체 안에는 id가 없음 - >에러
-        //historyLogService.logCreated(DomainType.USER, loggedInUserId, assignments, "프로젝트 회원 할당");
     }
 }
