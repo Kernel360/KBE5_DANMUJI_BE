97f4652e0bf1d1e88c6f1316d496d736d5170eeb
Jinhy00 <pjhark0@gmail.com>
2025-07-04 15:35:36 +0900

    refactor : interface -> record 변경 (#527)

--- module-core/src/main/java/com/back2basics/project/model/StatusCountProjection.java
+++ module-core/src/main/java/com/back2basics/project/model/StatusCountProjection.java
@@ -1,6 +1,8 @@
 package com.back2basics.project.model;
 
-public interface StatusCountProjection {
-    ProjectStatus getProjectStatus();
-    Long getCount();
+public record StatusCountProjection(
+    ProjectStatus projectStatus,
+    Long count
+) {
+
 }
