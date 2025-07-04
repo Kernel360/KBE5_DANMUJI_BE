914b2534465d35dca5ebba5eebde3e7102c3a372
swlee <swo98@naver.com>
2025-07-04 11:39:05 +0900

    fix: 불필요한 어노테이션 삭제

--- module-core/src/main/java/com/back2basics/infra/validation/custom/CustomLinkCheck.java
+++ module-core/src/main/java/com/back2basics/infra/validation/custom/CustomLinkCheck.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = CustomEnumCheckValidator.class)
+@Constraint(validatedBy = CustomLinkCheckValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CustomLinkCheck {
--- module-core/src/main/java/com/back2basics/infra/validation/custom/CustomNotBlank.java
+++ module-core/src/main/java/com/back2basics/infra/validation/custom/CustomNotBlank.java
@@ -2,13 +2,11 @@ package com.back2basics.infra.validation.custom;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
 @Constraint(validatedBy = CustomNotBlankValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
