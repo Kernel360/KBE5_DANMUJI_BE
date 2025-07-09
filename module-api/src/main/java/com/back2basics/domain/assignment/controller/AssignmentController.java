f1a5f03756e4f7e50b1204bf5d5a914e10367e90
Jinhy00 <pjhark0@gmail.com>
2025-07-09 12:22:20 +0900

    refactor: 주석추가(#532)

--- module-api/src/main/java/com/back2basics/domain/assignment/controller/AssignmentController.java
+++ module-api/src/main/java/com/back2basics/domain/assignment/controller/AssignmentController.java
@@ -1,6 +1,6 @@
 package com.back2basics.domain.assignment.controller;
 
-import static com.back2basics.domain.assignment.controller.code.AssignmentResponseCode.*;
+import static com.back2basics.domain.assignment.controller.code.AssignmentResponseCode.ASSIGNMENT_COMPANY_DELETE_SUCCESS;
 
 import com.back2basics.assignment.port.in.DeleteAssignmentUseCase;
 import com.back2basics.global.response.result.ApiResponse;
@@ -18,8 +18,10 @@ public class AssignmentController {
 
     private final DeleteAssignmentUseCase deleteAssignmentUseCase;
 
+    //todo: 사용안하면 추후 삭제
     @DeleteMapping("/{projectId}/companies/{companyId}")
-    public ResponseEntity<ApiResponse<Void>> deleteCompany(@PathVariable Long projectId, @PathVariable Long companyId) {
+    public ResponseEntity<ApiResponse<Void>> deleteCompany(@PathVariable Long projectId,
+        @PathVariable Long companyId) {
         deleteAssignmentUseCase.deleteCompanies(projectId, companyId);
         return ApiResponse.success(ASSIGNMENT_COMPANY_DELETE_SUCCESS);
     }
