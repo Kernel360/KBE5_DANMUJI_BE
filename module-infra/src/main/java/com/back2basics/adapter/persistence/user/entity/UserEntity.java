87724ad4420c3724fa8ca5751fed6460d8706b2b
swlee <swo98@naver.com>
2025-07-09 16:39:05 +0900

    feat: 멘션기능 테스트에 의해 유저엔티티에 username 인덱스 추가 (#567)

--- module-infra/src/main/java/com/back2basics/adapter/persistence/user/entity/UserEntity.java
+++ module-infra/src/main/java/com/back2basics/adapter/persistence/user/entity/UserEntity.java
@@ -11,6 +11,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,7 +24,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+    indexes =  @Index(name = "idx_user_username", columnList = "username")
+)
 public class UserEntity extends BaseTimeEntity {
 
     @Id
