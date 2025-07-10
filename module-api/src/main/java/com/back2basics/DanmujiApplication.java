6bae801d17853d74be3bbfc007e0cbdbc8d2ec80
ooyniz <ooyniz@gmail.com>
2025-07-10 14:35:42 +0900

    chore: @EnableScheduling 활성화

--- module-api/src/main/java/com/back2basics/DanmujiApplication.java
+++ module-api/src/main/java/com/back2basics/DanmujiApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.back2basics")
 @EntityScan("com.back2basics")
 @EnableJpaRepositories(basePackages = "com.back2basics")
 @EnableMongoRepositories(basePackages = "com.back2basics")
+@EnableScheduling
 public class DanmujiApplication {
 
     public static void main(String[] args) {
