f5a8f46f37a5f2baf50a5d735a61f76b383b9f94
Jinhy00 <pjhark0@gmail.com>
2025-07-16 12:30:05 +0900

    feat: 프로젝트 생성, 수정 validation 추가 (#560)

--- module-api/src/main/java/com/back2basics/domain/project/dto/request/ProjectCreateRequest.java
+++ module-api/src/main/java/com/back2basics/domain/project/dto/request/ProjectCreateRequest.java
@@ -1,16 +1,30 @@
 package com.back2basics.domain.project.dto.request;
 
+import com.back2basics.custom.CustomStringLengthCheck;
+import com.back2basics.custom.CustomUserDistinctCheck;
+import com.back2basics.custom.ManagerIncludedInUserCheck;
 import com.back2basics.project.port.in.command.ProjectCreateCommand;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.List;
 
-public record ProjectCreateRequest(@NotBlank(message = "프로젝트명은 필수입니다.") String name,
-                                   String description, String projectCost, LocalDate startDate, LocalDate endDate,
-                                   List<Long> devManagerId, List<Long> clientManagerId,
-                                   List<Long> devUserId,
-                                   List<Long> clientUserId
-) {
+@CustomUserDistinctCheck
+@ManagerIncludedInUserCheck
+public record ProjectCreateRequest(
+    @CustomStringLengthCheck(min = 1, max = 50, message = "프로젝트명은 50자 이내로 입력해주세요.")
+    String name,
+    @CustomStringLengthCheck(min = 1, max = 255, message = "프로젝트 개요는 255자 이내로 입력해주세요.")
+    String description,
+    String projectCost,
+    LocalDate startDate,
+    LocalDate endDate,
+    List<Long> devManagerId,
+    List<Long> clientManagerId,
+    @NotEmpty(message = "하나 이상의 개발사가 선택되어야 합니다.")
+    List<Long> devUserId,
+    @NotEmpty(message = "하나 이상의 고객사가 선택되어야 합니다.")
+    List<Long> clientUserId
+) implements UserDistinctCheckDto {
 
     public ProjectCreateCommand toCommand() {
         return ProjectCreateCommand.builder()
